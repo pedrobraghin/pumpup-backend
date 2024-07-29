555d4f3b10005f3e3e20f893209e15250cb61018
victorzix <linkgamesplays@gmail.com>
2024-07-29 13:24:54 -0300

    feat(main): add logger for application

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 const port = process.env.APP_PORT || 3000;
 
@@ -16,7 +17,17 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(port);
+  await app.listen(port, () => {
+    Logger.debug(
+      `Servidor rodando em http://localhost:${port}`,
+      'Application server',
+    );
+
+    Logger.debug(
+      `Documentação rodando em http://localhost:${port}/api`,
+      'Swagger',
+    );
+  });
 }
 
 bootstrap();
