c3f0af2b085665e0cb39aa00ad094fcea4f1bf41
victorzix <linkgamesplays@gmail.com>
2024-09-13 17:08:36 -0300

    refactor(train-history-exercise): change delete error handling method

--- src/modules/train-history-exercise/service/train-history-exercise.service.ts
+++ src/modules/train-history-exercise/service/train-history-exercise.service.ts
@@ -8,6 +8,7 @@ import { CreateTrainHistoryExerciseDTO } from '../dtos/requests/create-train-his
 import { TrainHistoryService } from '../../train-history/service/train-history.service';
 import { ExerciseService } from '../../exercise/services/exercise.service';
 import { UpdateTrainHistoryExerciseDTO } from '../dtos/requests/update-train-history-exercise.dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class TrainHistoryExerciseService {
@@ -73,8 +74,15 @@ export class TrainHistoryExerciseService {
   }
 
   async delete(userId: string, id: string) {
-    await this.getById(userId, id);
-
-    await this.trainHistoryExerciseRepository.delete(userId, id);
+    try {
+      await this.trainHistoryExerciseRepository.delete(userId, id);
+    } catch (err) {
+      if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        if (err.code === 'P2025') {
+          throw new NotFoundException('Train history exercise not found');
+        }
+      }
+      throw err;
+    }
   }
 }
