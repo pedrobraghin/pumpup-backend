48d33099c5a95e57281789cce67e43bf5fd41302
victorzix <linkgamesplays@gmail.com>
2024-10-05 12:08:01 -0300

    fix(seeds): fix seeds imports

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,13 @@
 import { PrismaClient } from '@prisma/client';
-import { users } from '../seeds/users.seed';
-import { exercises } from '../seeds/exercises.seed';
-import { createTrains } from '../seeds/trains.seed';
-import { createMuscleSeries } from '../seeds/muscle-serie.seed';
-import { createTrainHistories } from '../seeds/train-history.seed';
-import { createCardioSeries } from '../seeds/cardio-serie.seed';
-import { createTrainHistoryExercises } from '../seeds/train-history-exercise.seed';
-import { createTrainHistoryExerciseSeries } from '../seeds/train-history-exercise-serie.seed';
-import { createImages } from '../seeds/image.seed';
+import { users } from '../seeds/data/users.seed';
+import { exercises } from '../seeds/data/exercises.seed';
+import { createTrains } from '../seeds/data/trains.seed';
+import { createMuscleSeries } from '../seeds/data/muscle-serie.seed';
+import { createTrainHistories } from '../seeds/data/train-history.seed';
+import { createCardioSeries } from '../seeds/data/cardio-serie.seed';
+import { createTrainHistoryExercises } from '../seeds/data/train-history-exercise.seed';
+import { createTrainHistoryExerciseSeries } from '../seeds/data/train-history-exercise-serie.seed';
+import { createImages } from '../seeds/data/image.seed';
 
 const prisma = new PrismaClient();
 
